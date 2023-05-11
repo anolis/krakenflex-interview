8c50abf85d600887917e0e08472c05df33920ea3
Bill Carson <anolisporcatus@gmail.com>
2023-05-11 18:27:51 -0500

    add error handling for i/o issues related to api key

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const fs = require('fs');
 // import axios
 const axios = require('axios');
 
+
+
+try{
 //do this so we dont expose our api key in repo
 const API_KEY = fs.readFileSync('keys/api-key.txt', 'utf8');
-
+} catch (error) {
+  console.error('Error: API key file not found. Please add your API key to keys/api-key.txt');
+  return;
+}
 //our api root endpoint
 const BASE_URL = 'https://api.krakenflex.systems/interview-tests-mock-api/v1/';
 
@@ -20,7 +26,7 @@ const axiosConfig = {
 }
 
 if (API_KEY === '') {
-  console.error('Error: API key not found. Please add your API key to keys/api-key.txt');
+  console.error('Error: API key is null. Please check your API key in keys/api-key.txt');
   return;
 }
 
